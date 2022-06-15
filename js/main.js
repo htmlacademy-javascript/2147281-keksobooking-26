0d6d61a10ae582713eba86493416e4a0e2e607b0
thirdbyron <rgnatovsky@gmail.com>
2022-06-15 12:56:46 +0300

    Task2_finish

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-// Сделал через Math.abs, чтобы не плодить проверки на условия. Переделаю, если надо
+// Сделал через Math.abs, чтобы не плодить проверки на условия. Переделаю, если //надо
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+let randomInt;
+let randomFloat;
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно 
 function getRandomInt (min, max) {
-    if (min >= max) {
-        return 0
-    }
-    min = Math.floor(Math.abs(min))
-    max = Math.floor(Math.abs(max))
-    randomInt = Math.floor(Math.random() * (max - min + 1) + min)
-    return randomInt
+  if (min >= max) {
+    return 0;
+  }
+  min = Math.floor(Math.abs(min));
+  max = Math.floor(Math.abs(max));
+  randomInt = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomInt;
 }
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 function getRandomFloat (min, max, decimals) {
-    if (min >= max) {
-        return 0
-    };
-    min = Math.abs(min);
-    max = Math.abs(max);
-    randomFloat = Math.random() * (max - min + 1) + min;
-    return randomFloat.toFixed(decimals)
+  if (min >= max) {
+    return 0;
+  }
+  min = Math.abs(min);
+  max = Math.abs(max);
+  randomFloat = Math.random() * (max - min + 1) + min;
+  return randomFloat.toFixed(decimals);
 }
 
-getRandomInt(0, 5)
-getRandomFloat(0.5, 1.5)
+getRandomInt(0, 5);
+getRandomFloat(0.5, 1.5);
