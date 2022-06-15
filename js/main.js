305f4c26c95c5161061d65dd936ae2e800da485f
thirdbyron <rgnatovsky@gmail.com>
2022-06-15 12:32:20 +0300

    Task 2

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Сделал через Math.abs, чтобы не плодить проверки на условия. Переделаю, если надо
+
+// Функция, возвращающая случайное целое число из переданного диапазона включительно 
+function getRandomInt (min, max) {
+    if (min >= max) {
+        return 0
+    }
+    min = Math.floor(Math.abs(min))
+    max = Math.floor(Math.abs(max))
+    randomInt = Math.floor(Math.random() * (max - min + 1) + min)
+    return randomInt
+}
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+function getRandomFloat (min, max, decimals) {
+    if (min >= max) {
+        return 0
+    };
+    min = Math.abs(min);
+    max = Math.abs(max);
+    randomFloat = Math.random() * (max - min + 1) + min;
+    return randomFloat.toFixed(decimals)
+}
