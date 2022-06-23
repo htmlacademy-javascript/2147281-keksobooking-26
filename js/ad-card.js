63cec0f2942f9560d0b82b20fa88ade333a28c80
thirdbyron <rgnatovsky@gmail.com>
2022-06-23 23:56:10 +0300

    done

--- js/ad-card.js
+++ js/ad-card.js
@@ -37,33 +37,32 @@ const showDownloadedPhotos = (photoLinks, receivingContainer, receivingTemplate)
   }
 };
 
-const createAdCard = (adsVariety, index) => {
-  const singleAd = adsVariety[index];
+const createAdCard = (ad) => {
   const adClone = adTemplate.cloneNode(true);
   const title = adClone.querySelector('.popup__title');
-  title.textContent = singleAd.offer.title;
+  title.textContent = ad.offer.title;
   const adress = adClone.querySelector('.popup__text--address');
-  adress.textContent = singleAd.offer.adress;
+  adress.textContent = ad.offer.adress;
   const price = adClone.querySelector('.popup__text--price');
-  price.textContent = `${singleAd.offer.price} ₽/ночь`;
+  price.textContent = `${ad.offer.price} ₽/ночь`;
   const type = adClone.querySelector('.popup__type');
-  type.textContent = typesDictionary[singleAd.offer.type];
+  type.textContent = typesDictionary[ad.offer.type];
   const capacity = adClone.querySelector('.popup__text--capacity');
-  capacity.textContent = `Количество комнат: ${singleAd.offer.rooms}. Рассчитано на ${singleAd.offer.guests} ${overwriteGuestString(singleAd.offer.guests)}.`;
+  capacity.textContent = `Количество комнат: ${ad.offer.rooms}. Рассчитано на ${ad.offer.guests} ${overwriteGuestString(ad.offer.guests)}.`;
   const checkingTime = adClone.querySelector('.popup__text--time');
-  checkingTime.textContent = `Заезд после ${singleAd.offer.checkin}, выезд до ${singleAd.offer.checkout}`;
+  checkingTime.textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
   const description = adClone.querySelector('.popup__description');
-  if (singleAd.offer.description) {
-    description.textContent = singleAd.offer.description;
+  if (ad.offer.description) {
+    description.textContent = ad.offer.description;
   } else {
     description.style.display = 'none';
   }
   const avatar = adClone.querySelector('.popup__avatar');
-  avatar.src = singleAd.author.avatar;
+  avatar.src = ad.author.avatar;
   const features = adClone.querySelectorAll('.popup__feature');
-  keepExistingFeatures(features, singleAd.offer.features, 'popup__feature--');
+  keepExistingFeatures(features, ad.offer.features, 'popup__feature--');
   const photos = adClone.querySelector('.popup__photos');
-  showDownloadedPhotos(singleAd.offer.photos, photos, '.popup__photo');
+  showDownloadedPhotos(ad.offer.photos, photos, '.popup__photo');
   return adClone;
 };
 
