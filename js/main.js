4773fa9607f38b5a52f06746c7d62c5634cc3977
thirdbyron <rgnatovsky@gmail.com>
2022-06-27 22:25:49 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-import { createAdCard } from './ad-card.js';
 import { createAdsVariety } from './ads-generator.js';
 import { activeFormState } from './active-form-state.js';
+import { mainPinMarker, markerGroup, createAdMarker } from './map-markers.js';
+
 import './form-validation.js';
 
 activeFormState(false);
 
-const adsVariety = createAdsVariety();
-
-createAdCard(adsVariety[0]);
-
 const map = L.map('map').on('load', () => {
   activeFormState(true);
 }).setView({
-  lat: 35.6895,
-  lng: 139.692,
-}, 10);
+  lat: 35.67500,
+  lng: 139.75000,
+}, 13);
+
+const adressElement = document.querySelector('#address');
+
+const adsVariety = createAdsVariety();
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -23,16 +24,17 @@ L.tileLayer(
   },
 ).addTo(map);
 
-const marker = L.marker(
-  {
-    lat: 35.6895,
-    lng: 139.692,
-  },
-  {
-    draggable: true,
-  }
-);
+mainPinMarker.addTo(map);
 
-marker.addTo(map);
+mainPinMarker.on('moveend', (evt) => {
+  const lat = String(evt.target.getLatLng().lat.toFixed(5));
+  const lng = String(evt.target.getLatLng().lng.toFixed(5));
+  adressElement.value = `${lat}, ${lng}`;
+});
 
+markerGroup.addTo(map);
+
+adsVariety.forEach((ad) => {
+  createAdMarker(ad);
+});
 
